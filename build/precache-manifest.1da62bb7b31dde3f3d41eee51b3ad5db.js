51d61b16ac35c842cc7f93c456441848680fb2a9
Szörfi Jázmin <jazmin.szorfi@gmail.com>
2020-04-10 19:47:54 +0200

    Build

---
+++ build/precache-manifest.1da62bb7b31dde3f3d41eee51b3ad5db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e7342a2e967dc13b3b43b276b18801",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c6566bc90d7e93442c5c",
+    "url": "./static/css/main.e4e39ef2.chunk.css"
+  },
+  {
+    "revision": "894daed5cac13c915747",
+    "url": "./static/js/2.ee16ff66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ee16ff66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6566bc90d7e93442c5c",
+    "url": "./static/js/main.6700811f.chunk.js"
+  },
+  {
+    "revision": "821e3e9e8623f25acdce",
+    "url": "./static/js/runtime-main.62724d6a.js"
+  }
+]);
